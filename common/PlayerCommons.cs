a2acdb6881f4a4c01a80f77159e9f48a8a505273
davinjjimenez@gmail.com <davinjjimenez@gmail.com>
2019-08-23 22:37:18 -0700

    Added common folder, creating abstract class for Player object to inherit from

---
+++ common/PlayerCommons.cs
@@ -0,0 +1,50 @@
+/*
+    File: PlayerCommons.cs
+    Description: This file contains all the different variables all player objects will have in common such as name, 
+    hp, inventory, etc.
+    Purpose: This class allows other objects to inherit its property and define them as needed
+ */
+
+namespace cmdline_rpg.common
+{
+    public abstract class PlayerCommons
+    {
+        string name; // player name
+        int hp; // player hit points
+
+        /* 
+            Object types to add here later when implemented: 
+                1. Inventory object
+                2. Equipment object
+                3. Abilties/Spell object
+        */
+
+        // Constructor
+        public PlayerCommons(string playerName, int health) {
+            this.name = playerName;
+            this.hp = health;
+        }
+
+        // Attack command that will be determine a lot of other things by the Player object implemented later
+        public abstract void attack();
+        public abstract void defend();
+        public abstract void use(); // Will probably have an argument eventually, but no arguments for now
+
+        // Getters and setters
+        public string getName() {
+            return this.name;
+        }
+
+        public int getHp() {
+            return hp;
+        }
+
+        public void setName(string name) {
+            this.name = name;
+        }
+
+        public void setHp(int hp) {
+            this.hp = hp;
+        }
+    }
+}
