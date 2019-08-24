85cdc39989d62720789a5791534aefe60de44c49
davinjjimenez@gmail.com <davinjjimenez@gmail.com>
2019-08-24 15:22:01 -0700

    added small documentation to NPC.cs

--- Client/NPC/NPC.cs
+++ Client/NPC/NPC.cs
@@ -1,3 +1,11 @@
+/*
+File: NPC.cs
+Description: This file contains data related to NPCs in
+the game. It inherits from the abstract class NPCCommons.
+Purpose: This class serves as a base for all differnt types of NPCs. 
+Future NPC derivative classes can inherit from this class as necessary.
+*/
+
 using Client.Common;
 
 namespace Client.NPC
