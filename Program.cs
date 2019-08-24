d7a0ea7607c6e0495119348474834093a5bb4496
amnipp <andrewnipp@gmail.com>
2019-08-23 20:58:27 -0700

    Init proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cmdline_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
